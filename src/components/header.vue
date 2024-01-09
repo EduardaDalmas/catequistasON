28e3fbbb3f885f4a42c865cced7f562b5f181119
Eduarda Brisch Dalmás <du.dalmas@hotmail.com>
2024-01-09 16:33:03 -0300

    header e página inicial

---
+++ src/components/header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="header-menu row">
+    <img src="~assets/logo.png" alt="logo" class="logo" />
+    <div class="options row">
+      <div class="option">
+        <a href="/login">home</a>
+      </div>
+      <div class="option">
+        <a href="/register">Dinâmicas</a>
+      </div>
+      <div class="option">
+        <a href="/register">Contato</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'HeaderPage',
+})
+</script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,11 +1,29 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+  <q-parallax>
+    <template v-slot:media>
+      <img src="~assets/capa.png" class="imagem_tema">
+    </template>
+
+    <template v-slot:content="scope">
+      <div
+
+        :style="{
+          top: (scope.percentScrolled * 60) + '%',
+          left: 0,
+          right: 0
+        }"
+      >
+      <div class="text-overlay">“Deixem vir a mim as <span class="highlight"><b>crianças</b></span>, não as impeçam,
+        pois o Reino de Deus pertence aos que são
+        semelhantes a elas”. - Marcos 10, 14</div>
+      </div>
+    </template>
+  </q-parallax>
+
+  <div class="sesion">
+
+  </div>
+
 </template>
 
 <script>
@@ -14,4 +32,5 @@ import { defineComponent } from 'vue'
 export default defineComponent({
   name: 'IndexPage'
 })
+
 </script>
